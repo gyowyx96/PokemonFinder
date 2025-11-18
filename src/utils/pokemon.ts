f9f68ba0212ac8111dbf7e982fb128783a0222aa
giovanniLaScala <giovanni.lascala@stevejobs.academy>
2025-11-18 14:28:23 +0100

    add some pokemon

--- src/utils/pokemon.ts
+++ src/utils/pokemon.ts
@@ -1,10 +1,12 @@
 export const pokemonSelect = () => {
     const target = [
-      {name: "crustle", pos: {x: 887, y: 286}, shiny: true },
-      {name: "regigigas", pos: {x: 232, y: 580}, shiny: true },
+      {name: "crustle", pos: {x: 887, y: 286}, },
+      {name: "regigigas", pos: {x: 232, y: 580}, shiny: false },
       {name: "braviary", pos: {x: 1077, y: 336}},
       {name: "feebas", pos: {x: 918, y: 498} },
-      {name: "charizard", pos: {x: 887, y: 286} },
+      {name: "charizard", pos: {x: 274, y: 306} },
+      {name: "bronzong", pos: {x:1100, y: 456}, shiny: true },
+      {name: "lapras", pos: {x:1272, y:205} },
     ]
     const getRandomElement = <T,>(arr: T[]) => {
       const randomIndex = Math.floor(Math.random() * arr.length);
